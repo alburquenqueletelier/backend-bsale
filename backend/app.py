43a23efcc6c2ccd517d4ffd14e9f7a7cf37103c9
Bryan Alburquenque <alburquenque.letelier@gmail.com>
2022-11-08 16:11:20 -0300

    Endpoint actualizados: reciben arguementos para filtro

--- backend/app.py
+++ backend/app.py
@@ -13,16 +13,25 @@ app.config['MYSQL_DB'] = 'bsale_test'
 mysql = MySQL(app)
 
 @app.route("/")
-def hello_world():
-    
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template("index.html")
 
 @app.route("/category")
 def get_categories():
+    id = request.args.get('id', None)
+    name = request.args.get('name', None)
     try:
         cursor = mysql.connection.cursor()
-        cursor.execute('''SELECT * from category''')
+        if id:
+            cursor.execute(f'''SELECT * FROM category WHERE id="{id}" LIMIT 1''')
+        elif name:
+            name=name.replace('+', ' ')
+            print(name)
+            cursor.execute(f'''SELECT * FROM category WHERE name="{name}" LIMIT 1''')
+        else:
+            cursor.execute('''SELECT * FROM category''')
         rv = cursor.fetchall()
+        print(rv)
         response = []
         for row in rv:
             response.append({"id": row[0], "name": row[1]})
@@ -33,17 +42,30 @@ def get_categories():
 
 @app.route("/product")
 def get_products():
+    id = request.args.get('id', None)
+    name = request.args.get('name', None)
+    category = request.args.get('category', None) # int
     try:
         cursor = mysql.connection.cursor()
-        cursor.execute('''SELECT * from product''')
+        if id:
+            print(id)
+            cursor.execute(f'''SELECT * FROM product WHERE id="{id}" LIMIT 1''')
+        elif name:
+            cursor.execute(f'''SELECT * from product WHERE name="{name}"''')
+        elif category:
+            cursor.execute(f'''SELECT * from product WHERE category="{category}"''')
+        else:
+            cursor.execute('''SELECT * from product''')
         rv = cursor.fetchall()
+        if not rv:
+            return jsonify({"empty": "No se encuentra lo que buscas"})
         response = []
         for row in rv:
             response.append({"id": row[0], "name": row[1], "url_image": row[2], "price": row[3], "discount": row[4], "category": row[5]})
         cursor.close()
         return jsonify(response), 200
-    except:
-        return jsonify({"error": "No fue posible conseguir la data"}), 500
+    except Exception as e:
+        return jsonify({"error": e}), 500
 
 
 if __name__ == '__main__':
