6715e8d533c228ea5552ff8d09d1003a42685bcc
Bryan Alburquenque <alburquenque.letelier@gmail.com>
2022-11-08 21:10:30 -0300

    Actualizado, se agrega readme

--- backend/app.py
+++ backend/app.py
@@ -9,7 +9,8 @@ app.config['MYSQL_HOST'] = 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com'
 app.config['MYSQL_USER'] = 'bsale_test'
 app.config['MYSQL_PASSWORD'] = 'bsale_test'
 app.config['MYSQL_DB'] = 'bsale_test'
- 
+
+CORS(app)
 mysql = MySQL(app)
 
 @app.route("/")
